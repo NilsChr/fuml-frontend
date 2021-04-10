b6ebd8d24f5f9381325795383707e3c331d23d8a
Nils Christian Bogen <nils.chr.bogen@gmail.com>
2021-04-10 10:56:34 +0200

    added style to sequence digram

--- src/services/umlParser/sequence.parser.js
+++ src/services/umlParser/sequence.parser.js
@@ -1,5 +1,27 @@
 function sequenceUMLParser(document) {
   let code = "";
+  code += "skinparam useBetaStyle true\n";
+  code += "skinparam roundCorner 5\n";
+  code += "skinparam linetype polyline\n";
+  code += "skinparam linetype ortho\n";
+
+  code += `
+  skinparam sequence {
+    ArrowColor black
+    ActorBorderColor black
+    LifeLineBorderColor black
+    LifeLineBackgroundColor white
+    
+    ParticipantBorderColor Black
+    ParticipantBackgroundColor White
+    ParticipantFontSize 17
+    ParticipantFontColor black
+    
+    ActorBackgroundColor white
+    ActorFontColor black
+    ActorFontSize 17
+    }
+      `;
 
   for (let i = 0; i < document.participants.length; i++) {
     let participant = document.participants[i];
@@ -8,9 +30,8 @@ function sequenceUMLParser(document) {
 
   for (let i = 0; i < document.parts.length; i++) {
     let part = document.parts[i];
-    if(!part.visible) continue;
-    if(part.block)
-    code +=  'Group \"' + part.title + "\"\n";
+    if (!part.visible) continue;
+    if (part.block) code += 'Group "' + part.title + '"\n';
 
     /* TODO: ADD THIS TO ENABLE SPACE IN NAMES
     let tempCode = part.code;
@@ -21,13 +42,10 @@ function sequenceUMLParser(document) {
     code += tempCode + "\n";
     console.log(code);
     */
-    code +=  part.code + "\n";
+    code += part.code + "\n";
 
-    if(part.block)
-    code += 'end\n';
+    if (part.block) code += "end\n";
   }
-
-
 
   return code;
 }
